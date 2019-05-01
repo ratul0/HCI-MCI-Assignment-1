8bb800bb1d43f262b152b5af1cd5c7e8d48a822b
Yousuf Khan <ratulcse27@gmail.com>
2019-05-01 19:24:15 +0200

    add masters offers

--- prepareLocalStorage.js
+++ prepareLocalStorage.js
@@ -45,6 +45,39 @@ function prepareLocalStorage(inDevelopment){
        }
     };
     localStorage.setItem('bachelor_externalLinks', JSON.stringify(bachelor_externalLinks));
+
+    var master_externalLinks = {
+      apsm: {
+       title: "Applied Computer Science",
+       href: "https://www.uni-bamberg.de/en/ma-ai/"
+      },
+      ch: {
+       title: "Computing in the Humanities",
+       href: "https://www.uni-bamberg.de/en/ma-cith/"
+      },
+      iism: {
+       title: "International Information Systems Management (information in German)",
+       href: "https://www.uni-bamberg.de/en/ma-iism/"
+      },
+      isss: {
+        title: "International Software Systems Science",
+        href: "https://www.uni-bamberg.de/en/ma-isosysc/"
+       },
+
+       is: {
+        title: "Information Systems",
+        href: "https://www.uni-bamberg.de/en/ma-wi/"
+       },
+       ebis: {
+        title: "Education in Business and Information Systems",
+        href: "https://www.uni-bamberg.de/en/wipaed-wi/"
+       },
+       vatpis: {
+        title: "Virtual Advanced Training Programme in Information Systems (information in German)",
+        href: "https://www.uni-bamberg.de/ma-vawi/"
+       }
+    };
+    localStorage.setItem('master_externalLinks', JSON.stringify(master_externalLinks));
     
     
     // ****  Prepare Local Storage for Other Screens
